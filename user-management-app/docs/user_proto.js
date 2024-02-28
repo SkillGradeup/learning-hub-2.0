d5158b2822c1c54ffd45313a7557bfd966fc4860
sagarapidev <106273002+sagarapidev@users.noreply.github.com>
2024-02-29 02:04:49 +0530

    Update user_proto.js

--- user-management-app/docs/user_proto.js
+++ user-management-app/docs/user_proto.js
@@ -1,3 +1,16 @@
-function getAuth(){
-    {};[];"test"
+// Create a new user
+async function isCodeScanTriggered(userId) {
+  try {
+    const pool = sql.connect(dbConfig);
+    const sqlString = `SELECT * FROM users WHERE UserID = ${userId}`;
+    const result = await pool.request().query(sqlString);
+    return result.recordset[0]; // Assuming ID is unique, so return the first result
+  } catch (error) {
+    const errId=userId;
+    console.error(
+      `Error fetching user with ID ${errId} from the database:`,
+      error
+    );
+    throw error;
+  }
 }
